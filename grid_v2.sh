0362d204d3437bed44ea4958b5a5a4269ac52888
Marcin Przewięźlikowski <m.przewie@gmail.com>
2021-11-23 20:15:21 +0100

    new gridsearch

--- grid_v2.sh
+++ grid_v2.sh
@@ -10,29 +10,34 @@ which python
 
 CMD="python train.py --dataset cross_char --num_classes 4112 --method hn_poc --train_n_way 5 --seed 1 --resume"
 
-
-for HN_HIDDEN_SIZE in 512;
+for DETACH_EPOCH in 100000 250;
 do
-  for HN_TN_HIDDEN_SIZE in 256 128;
+  for HN_HIDDEN_SIZE in 1024 512;
   do
-    for HN_TASKSET_SIZE in 8 4 1;
+    for HN_TN_HIDDEN_SIZE in 512 256;
     do
-      for HN_NECK_LEN in 1 0;
+      for HN_TN_DEPTH in 1 2 3;
       do
-        for HN_HEAD_LEN in 2 1;
+        for HN_NECK_LEN in 1 0;
         do
-          for LR in 1e-2 1e-3 1e-4;
+          for HN_HEAD_LEN in 1;
           do
-            SUFFIX="${HN_HIDDEN_SIZE}-${HN_TN_HIDDEN_SIZE}-${HN_TASKSET_SIZE}-${HN_NECK_LEN}-${HN_HEAD_LEN}_${LR}_long"
-            $CMD --hn_hidden_size $HN_HIDDEN_SIZE \
-              --hn_tn_hidden_size $HN_TN_HIDDEN_SIZE \
-              --hn_taskset_size $HN_TASKSET_SIZE \
-              --hn_neck_len $HN_NECK_LEN \
-              --hn_head_len $HN_HEAD_LEN \
-              --stop_epoch 1000 \
-              --checkpoint_suffix $SUFFIX &
+            for LR in 1e-3 1e-4;
+            do
+              SUFFIX="${HN_HIDDEN_SIZE}-${HN_TN_HIDDEN_SIZE}-${HN_TASKSET_SIZE}-${HN_NECK_LEN}-${HN_HEAD_LEN}_${HN_TN_DEPTH}_${LR}_long"
+              $CMD --hn_hidden_size $HN_HIDDEN_SIZE \
+                --hn_tn_hidden_size $HN_TN_HIDDEN_SIZE \
+                --hn_taskset_size $HN_TASKSET_SIZE \
+                --hn_neck_len $HN_NECK_LEN \
+                --hn_head_len $HN_HEAD_LEN \
+                --hn_tn_depth $HN_TN_DEPTH \
+                --hn_detach_ft_in_hn $DETACH_EPOCH \
+                --hn_detach_ft_in_tn $DETACH_EPOCH \
+                --stop_epoch 1000 \
+                --checkpoint_suffix $SUFFIX
+            done
+            wait
           done
-          wait
         done
       done
     done
