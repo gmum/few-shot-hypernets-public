3d007ec5b2b6303243406a604d95cff6f142cc27
Marcin Przewięźlikowski <m.przewie@gmail.com>
2021-12-19 19:23:00 +0100

    grid scripts

--- grid.sh
+++ grid.sh
@@ -11,15 +11,15 @@ which python
 CMD="python train.py --dataset cross_char --num_classes 4112 --method hn_poc --train_n_way 5 --seed 1 --resume"
 
 
-for HN_HIDDEN_SIZE in 128 256 512;
+for HN_HIDDEN_SIZE in 512 256 128;
 do
-  for HN_TN_HIDDEN_SIZE in 64 128 256;
+  for HN_TN_HIDDEN_SIZE in 256 128 64;
   do
-    for HN_TASKSET_SIZE in 1 4 8;
+    for HN_TASKSET_SIZE in 8 4 1;
     do
-      for HN_NECK_LEN in 0 1 2 3;
+      for HN_NECK_LEN in 0 1 2;
       do
-        for HN_HEAD_LEN in 1 2 3;
+        for HN_HEAD_LEN in 1 2;
         do
           SUFFIX="${HN_HIDDEN_SIZE}-${HN_TN_HIDDEN_SIZE}-${HN_TASKSET_SIZE}-${HN_NECK_LEN}-${HN_HEAD_LEN}"
           $CMD --hn_hidden_size $HN_HIDDEN_SIZE \
@@ -27,10 +27,14 @@ do
             --hn_taskset_size $HN_TASKSET_SIZE \
             --hn_neck_len $HN_NECK_LEN \
             --hn_head_len $HN_HEAD_LEN \
-            --checkpoint_suffix $SUFFIX
-
+            --checkpoint_suffix $SUFFIX #&
+		
         done
+	
+      #wait # wait for the innermost loop 
+
       done
+      #wait
     done
   done
 done
