4231502df6bcbfb5e3a6d6d762357c69054643cb
Marcin Przewięźlikowski <m.przewie@gmail.com>
2021-12-23 11:23:50 +0100

    log acc during training

--- train.py
+++ train.py
@@ -54,6 +54,7 @@ def train(base_loader, val_loader, model, optimization, start_epoch, stop_epoch,
             os.makedirs(params.checkpoint_dir)
 
         acc = model.test_loop(val_loader)
+        print("max acc", max_acc, "acc", acc)
         if acc > max_acc:  # for baseline and baseline++, we don't use validation here so we let acc = -1
             print("--> Best model! save...")
             max_acc = acc
