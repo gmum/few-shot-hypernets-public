8f4a779283f15e32567da812d66997c007d27269
OneAndZero24 <oneandzero24@gmail.com>
2022-08-14 22:56:23 +0200

    .

--- utils.py
+++ utils.py
@@ -35,7 +35,7 @@ def kl_diag_gauss_with_standard_gauss(mean, logvar):
     logvar_flat = torch.cat([t.view(-1) for t in logvar])
     var_flat = logvar_flat.exp()
 
-    return -0.5 * torch.sum(torch.ones(logvar_flat.size(dim:0)) + logvar_flat - mean_flat.pow(2) - var_flat)
+    return -0.5 * torch.sum(torch.ones(logvar_flat.size(dim=0)) + logvar_flat - mean_flat.pow(2) - var_flat)
 
 def reparameterize(mu, logvar):
     std = torch.exp(0.5 * logvar)
