bf01f4a9279e80d300cb4e18ed96f4d2db671c30
OneAndZero24 <oneandzero24@gmail.com>
2022-09-24 22:06:08 +0200

    bruh

--- methods/hypernets/hypernet_poc.py
+++ methods/hypernets/hypernet_poc.py
@@ -342,7 +342,8 @@ class HyperNetPOC(MetaTemplate):
 
                     if tr == 0:
                         for k, p in get_param_dict(self).items():
-                            metrics[f"grad_norm/{k}"] = p.grad.abs().mean().item() if p.grad is not None else 0
+                            if(k.split('.')[0] != "target_net_architecture"):
+                                metrics[f"grad_norm/{k}"] = p.grad.abs().mean().item() if p.grad is not None else 0
 
                     optimizer.step()
 
