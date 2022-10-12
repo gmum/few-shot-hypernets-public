cdcbc1913d614bb1fd8d53e1d2b01b7a6d1c0ef8
OneAndZero24 <oneandzero24@gmail.com>
2022-10-12 19:54:35 +0200

    no reparam scalling

--- backbone.py
+++ backbone.py
@@ -132,21 +132,21 @@ class BayesLinear2(nn.Module):
     #         torch.nn.init.uniform_(self.bias_log_var, -bound, bound)
 
     def update_scale(self):
-        if self.param_scale is not None and self.param_scale + self.step_reparam < 1.0:
+        if self.step_reparam is not None and self.param_scale + self.step_reparam < 1.0:
             self.param_scale += self.step_reparam
         else:
             self.param_scale = 1.0
 
     def forward(self, x):
 
-        if self.start_reparam <= self.calls_counter:
-            self.update_scale()
+        #if self.start_reparam <= self.calls_counter:
+        #    self.update_scale()
 
-        self.calls_counter = self.calls_counter + 1
+        #self.calls_counter = self.calls_counter + 1
 
         if self.training and self.bayesian:
-            weight = reparameterize(self.weight_mu, self.weight_log_var, self.param_scale)
-            bias = reparameterize(self.bias_mu, self.bias_log_var, self.param_scale)
+            weight = reparameterize(self.weight_mu, self.weight_log_var, 1.0)
+            bias = reparameterize(self.bias_mu, self.bias_log_var, 1.0)
             return F.linear(x, weight, bias)
         else:
             return F.linear(x, self.weight_mu, self.bias_mu)
