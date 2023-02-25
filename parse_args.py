dfc69dde3793a9f367f36ae48c2b46534700ac28
Mateusz Rajski <mateuszrajski@Mateuszs-MacBook-Air.local>
2023-02-25 22:40:04 +0100

    Improve param generation

--- parse_args.py
+++ parse_args.py
@@ -8,9 +8,18 @@ def read_args():
         args_dict = dict(json.load(json_args))
         return args_dict
     
+def param_form(k, v):
+    if type(v) == bool and v:
+        return f'--{k}'
+    
+    if not v:
+        return ''
+    
+    return f'--{k}={v}'
+    
 def create_params():
     args_dict = read_args()
-    arguments = [f'--{k}={v}' for k, v in args_dict]
+    arguments = [param_form(k,v) for k, v in args_dict]
     return ' '.join(arguments)
 
 if __name__ == '__main__':
