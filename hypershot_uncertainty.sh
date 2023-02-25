6557daa5a68e85b244ebc4a1bc3613d777f3f065
Mateusz Rajski <mateuszrajski@Mateuszs-MacBook-Air.local>
2023-02-25 22:09:26 +0100

    Fix of bash script

--- hypershot_uncertainty.sh
+++ hypershot_uncertainty.sh
@@ -1,8 +1,5 @@
-
-
-BASE_PATH = $1
-export ARGS_PATH = "$BASE_PATH/args.json"
-export MODEL_PATH = "$BASE_PATH/best_model.tar"
+export ARGS_PATH = "$1/args.json"
+export MODEL_PATH = "$1/best_model.tar"
 
 source activate few-shot-learning
 
