0e8df663d5d541f1d6d21a14b9c4bdfb64b4aa00
OneAndZero24 <oneandzero24@gmail.com>
2023-02-28 20:25:54 +0100

    Experiment done

--- hypershot_uncertainty.py
+++ hypershot_uncertainty.py
@@ -1,9 +1,16 @@
-import os
-import torch
-from io_utils import setup_neptune, model_dict, parse_args
-from methods.hypernets.hypernet_kernel import HyperShot
 import json
+import os
+from functools import reduce
 
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from neptune.new.types import File
+
+import configs
+from data.datamgr import SetDataManager
+from io_utils import model_dict, parse_args, setup_neptune
+from methods.hypernets.hypernet_kernel import HyperShot
 
 # NOTE: This uncertainty experiment was created on the master branch.
 # But still we have to use it on other branches with different implementations of model architectures (and different set of parameters).
@@ -17,32 +24,77 @@ import json
 
 # NOTE: To run an experiment just copy a file `experiment_template.sh` and customize it.
  
-def create_model_instance(params):
+def train_fs_params(params):
     n_query = max(1, int(16 * params.test_n_way / params.train_n_way))
-    train_few_shot_params = dict(n_way=params.train_n_way, n_support=params.n_shot, n_query=n_query)
-    return HyperShot(model_dict[params.model], params=params, **train_few_shot_params).cuda()
+    return dict(n_way=params.train_n_way, n_support=params.n_shot, n_query=n_query)
 
-def experiment():
+def create_model_instance(params):
+    return HyperShot(model_dict[params.model], params=params, **train_fs_params(params)).cuda()
+
+def load_dataset(params):
+    file = configs.data_dir[params.dataset] + 'base.json'
+    if params.dataset == 'cross':
+        file = configs.data_dir['miniImagenet'] + 'all.json'
+    elif params.dataset == 'cross_char':
+        file = configs.data_dir['omniglot'] + 'noLatin.json'
+
+    image_size = 224
+    if params.dataset in ['omniglot', 'cross_char']:
+        image_size = 28
+    else:
+        image_size = 84
+
+    data_mgr = SetDataManager(image_size, **train_fs_params(params))
+    return iter(data_mgr.get_data_loader(file, aug=False))
+
+def upload_hist(neptune_run, arr, i):
+    fig = plt.figure()
+    plt.hist(arr, edgecolor="black", range=[0, 1], bins=25)
+    mu = np.mean(arr)
+    std = np.std(arr)
+    plt.title(f'$\mu = {mu:.3}, \sigma = {std:.3}$')
+    neptune_run[f"Histogram{i}"].upload(File.as_image(fig))
+    plt.close(fig)
+
+def experiment(N):
     params = parse_args('train') # We need to parse the same parameters as during training
-    model_path = os.environ.get('MODELPATH')
+    neptune_run = setup_neptune(params)
 
+    model_path = os.environ.get('MODELPATH')
 
     # Load model
     model = create_model_instance(params)
     tmp = torch.load(model_path)
     model.load_state_dict(tmp['state'])
+    model.eval()
 
+    dataset = load_dataset(params)
 
-    print(model.S)
+    def take_next():
+        return next(dataset, (None, None))
 
-    #Parameters: N, M, model, dataset
-    #1. Load Model
-    #2. Load dataset [(S, Q)]
-    #3. For N (S, Q) pairs
-    #   - Select ith (S, Q) pair
-    #   - Select another one with disjoint support (S', Q')
-    #   - Eval model M times on (S, Q') and generate histogram tagged as ith
+    def cond(x, y):
+        return (x is not None) and (y is not None)
 
+    X = []
+    Y = []
+    x, y = take_next()
+    while cond(x, y):
+        Y.append(x)
+        X.append(y)
+        x, y = take_next()
+        while (reduce(np.intersect1d, (*Y, y)).size > 0) and cond(x, y): 
+            x, y = take_next()
+        
+    S, Q = model.parse_feature(X, is_feature=False)
+    i = 0
+    for s, q in zip(enumerate(S), enumerate(Q)):
+        classifier, _ = model.generate_target_net(s)
+        r = []
+        for _ in range(N):
+            r.append(torch.nn.functional.softmax(classifier(q), dim=1)[0].clone().data.cpu().numpy())
+        upload_hist(neptune_run, r, i)
+        i += 1
 
 if __name__ == '__main__':
-    experiment()
+    experiment(30)
