89bc33bd716da700858fe3e7f3559406c72cd2b6
OneAndZero24 <oneandzero24@gmail.com>
2023-03-03 18:58:36 +0100

    dimension fix?

--- hypershot_uncertainty.py
+++ hypershot_uncertainty.py
@@ -92,7 +92,7 @@ def experiment(N):
         
     S = torch.Tensor()
     Q = torch.Tensor()
-    for xi in X:
+    for xi in X.flatten().chunk(100):
         s, q = model.parse_feature(xi, is_feature=False)
         S = torch.cat((S, s), 0)
         Q = torch.cat((Q, q), 0)
