82df342064a040abdf47dde90c6e9d8b6aa0c6d7
OneAndZero24 <oneandzero24@gmail.com>
2023-03-04 15:17:54 +0100

    .

--- hypershot_uncertainty.py
+++ hypershot_uncertainty.py
@@ -97,7 +97,7 @@ def experiment(N):
     ims = get_image_size(params)
     bs =  model.n_way*(model.n_support + model.n_query)*ims*ims
     bn = int(torch.numel(X)/bs)
-    B = torch.chunk(X.flatten(), bn)
+    B = torch.chunk(X, bn)
 
     S = torch.Tensor()
     Q = torch.Tensor()
