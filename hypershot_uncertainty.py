1cd0f5d3553b5eb70b4b50cfe381410bda7bf7d4
OneAndZero24 <oneandzero24@gmail.com>
2023-03-04 15:25:30 +0100

    fix?

--- hypershot_uncertainty.py
+++ hypershot_uncertainty.py
@@ -101,7 +101,7 @@ def experiment(N):
 
     S = torch.Tensor()
     Q = torch.Tensor()
-    for b in B:
+    for b in B[:-1]:
         s, q = model.parse_feature(b, is_feature=False)
         S = torch.cat((S, s), 0)
         Q = torch.cat((Q, q), 0)
