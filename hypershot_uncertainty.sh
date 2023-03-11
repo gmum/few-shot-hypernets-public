aa8eef5a0c601c07c1a89ccb22a1db62d49d9f68
Mateusz Rajski <mateuszrajski@Mateuszs-MacBook-Air.local>
2023-03-11 15:21:09 +0100

    Print parsed args

--- hypershot_uncertainty.sh
+++ hypershot_uncertainty.sh
@@ -5,4 +5,7 @@ export MODELPATH="$1/best_model.tar"
 echo "ARGSPATH=$ARGSPATH"
 echo "MODELPATH=$MODELPATH"
 
+echo "MODEL ARGUMENTS:"
+echo $(python parse_args.py)
+
 python hypershot_uncertainty.py $(python parse_args.py)
