a439e616d74b5e2b46edeadffc087ff1960faf52
Marcin Przewięźlikowski <m.przewie@gmail.com>
2024-04-02 14:01:26 +0200

    add bayesian hypershot commands

--- commands.sh
+++ commands.sh
@@ -34,6 +34,26 @@ python train.py --method hyper_shot --model Conv4 --dataset CUB --num_classes 20
   --hn_tn_depth 2  --hn_head_len 2 --hn_neck_len 0 --hn_hidden_size 128 --hn_tn_hidden_size 64 \
   --hn_sup_aggregation mean --hn_use_cosine_distance
 
+# Bayesian HyperShot
+
+## cross_char
+### 1-shot
+
+python train.py --method hyper_shot --hn_bayesian_model  --hn_bayesian_test --model Conv4 --dataset cross_char --num_classes 4112 \
+  --n_shot 1 --test_n_way 5 --train_n_way 5--stop_epoch 2000 --hn_val_epochs 0 --hn_val_lr 0.01 \
+  --hn_head_len 2 --hn_neck_len 1 --hn_hidden_size 512 --hn_tn_hidden_size 128 --hn_tn_depth 2  \
+  --hn_use_kld --hn_S=3 --hn_use_cosine_distance --hn_kld_const_scaler -4 --hn_S_test=3 --hn_use_mu_in_kld
+
+
+## cross
+### 5-shot
+
+python train.py --dataset miniImagenet --n_shot 5 --test_n_way 5 --num_classes 200 --method hyper_shot\
+ --train_n_way 5 --seed 1 --resume --train_aug --hn_use_cosine_distance --hn_hidden_size 4096 --hn_tn_hidden_size 1024\
+  --hn_taskset_size 1 --hn_neck_len 2 --hn_head_len 3 --hn_tn_depth 1 --hn_detach_ft_in_hn 100000\
+   --hn_detach_ft_in_tn 100000 --stop_epoch 10001 --es_epoch 10000 --save_freq 500 --hn_val_epochs 0 --hn_use_kld --hn_S 5\
+     --hn_kld_const_scaler 0 --hn_S_test 10 --hn_bayesian_model --hn_bayesian_test --maybe_terminate\
+      --detect_anomaly --hn_use_kld_scheduler --hn_kld_start_val -24 --hn_use_mu_in_kld --hn_kld_stop_val -3
 
 # HyperMAML
 
